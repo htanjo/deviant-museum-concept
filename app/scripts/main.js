d3cce65c2e53887b55536249eeb6d7a65302b162
Tanjo, Hiroyuki <expheno@gmail.com>
2014-06-15 20:58:30 +0900

    Implement wasd move

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,37 +5,117 @@
   var requestAnimationFrame = window.requestAnimationFrame;
 
   var scene, camera, renderer;
-  var geometry, material, mesh;
+  var plane;
+
+  var input;
 
   function init() {
 
     scene = new THREE.Scene();
+    input = new Input();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
-
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-    mesh = new THREE.Mesh( geometry, material );
-
-    scene.add( mesh );
-
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    initCamera();
+    initMesh();
+    initRenderer();
 
     document.body.appendChild( renderer.domElement );
 
   }
 
+  function initCamera() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.y = 100;
+    camera.position.z = 1000;
+
+  }
+
+  function initMesh() {
+
+    var geometry = new THREE.PlaneGeometry(10000, 10000);
+    var material = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide, wireframe: true});
+    plane = new THREE.Mesh(geometry, material);
+
+    plane.rotation.x = 90 / 180 * Math.PI;
+
+    scene.add(plane);
+
+  }
+
+  function initRenderer() {
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+  }
+
   function animate() {
 
-    // note: three.js includes requestAnimationFrame shim
+    var speed = 10;
+
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    if (input.up) {
+      camera.position.z -= speed;
+    }
+    if (input.down) {
+      camera.position.z += speed;
+    }
+    if (input.left) {
+      camera.position.x -= speed;
+    }
+    if (input.right) {
+      camera.position.x += speed;
+    }
 
     renderer.render( scene, camera );
+
+  }
+
+  function Input() {
+    var self = this;
+
+    function onKeyDown(event) {
+      switch (event.keyCode) {
+        case 87:    // w
+          self.up = true;
+          break;
+        case 83:    // s
+          self.down = true;
+          break;
+        case 65:    // a
+          self.left = true;
+          break;
+        case 68:    // d
+          self.right = true;
+          break;
+      }
+    }
+
+    function onKeyUp(event) {
+      switch (event.keyCode) {
+        case 87:    // w
+          self.up = false;
+          break;
+        case 83:    // s
+          self.down = false;
+          break;
+        case 65:    // a
+          self.left = false;
+          break;
+        case 68:    // d
+          self.right = false;
+          break;
+      }
+    }
+
+    this.up = false;
+    this.down = false;
+    this.left = false;
+    this.right = false;
+
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
 
   }
 
